506f2e302009b1c10da950d84d148dc17210fe2e
ImperioKDB <kingbrite123@gmail.com>
2026-05-05 19:48:12 +0100

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // REMOVE THIS: output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.supabase.co', pathname: '/storage/v1/object/public/**' }
