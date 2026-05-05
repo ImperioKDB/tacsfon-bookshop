7062bcead129af57a9e75ac4017beb2f842bbbe8
ImperioKDB <kingbrite123@gmail.com>
2026-05-05 10:47:26 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1a56db",
+        secondary: "#7e3af2",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
